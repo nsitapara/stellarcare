a07f49e6a4e12004b766221a6fdd767767e0024e
nsitapara <nsitapara@gmail.com>
2025-01-03 12:59:16 -0800

    reinstall next in frontend and remove nested setup

--- frontend/apps/web/providers/auth-provider.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { SessionProvider } from 'next-auth/react'
-import type { PropsWithChildren } from 'react'
-
-export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>
-}
